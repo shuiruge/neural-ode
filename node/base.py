76e2d6016520a0b7aac54c3f22a3c01008654b91
shuiruge <shuiruge@hotmail.com>
2019-12-15 22:34:40 +0800

    Fix bug.

--- node/base.py
+++ node/base.py
@@ -10,8 +10,8 @@ class ODESolver:
     r"""
     Definition
     ----------
-    $\text{ode_solver}(f, t_0, t_1, z(t_0)) := z(t_0) + \int_{t_0}^{t_1} f(z(t), t) dt$  # noqa:E501
-    which is exectly the $z(t_1)$.
+    $\text{ode_solver}(f, t_0, t_N, z(t_0)) := z(t_0) + \int_{t_0}^{t_N} f(z(t), t) dt$  # noqa:E501
+    which is exectly the $z(t_N)$.
     """
 
     def __call__(self, phase_vector_field):
@@ -38,53 +38,38 @@ class ODESolver:
 
 
 # TODO: re-write
-def reverse_mode_derivative(ode_solver,
-                            network,
-                            var_list,
-                            start_time,
-                            end_time,
-                            final_state,
-                            loss_gradient):
+def reverse_mode_derivative(ode_solver, network, variables):
     r"""Implements the algorithm 1 in the paper original paper (1806.07366).
 
     Args:
         ode_solver: ODESolver
         network: Callable[[tf.Tensor, Time], tf.Tensor]
             The $f(x, t)$ in the paper.
-        var_list: List[tf.Variable]
+        variables: List[tf.Variable]
             The $\theta$ in the paper. In practice, it's a list of variables.
             Thus $\theta = (\theta_1, \ldots)$,
-        start_time: Time
-        end_time: Time
-        final_state: tf.Tensor
-            The $z^{\alpha}(t_1)$ in the paper. The final outputs of the
-            Neural ODE.
-        loss_gradient: tf.Tensor
-            The $\frac{\partial L}{\partial z^{\alpha}(t_1)}$ in the paper.
-            The gradient of loss by the final output of the Neural ODE
-            (i.e. by the `final_state`).
 
-    Returns: tf.Tensor
-        The $\frac{\partial L}{\partial \theta_i}$ for the variable
-        $\theta_i$ in the variable list $\theta$.
+    Returns: Callable for backward propagation
+        Args:
+            start_time: Time
+            end_time: Time
+            final_state: tf.Tensor
+                The $z^{\alpha}(t_N)$ in the paper. The final outputs of the
+                Neural ODE.
+            final_loss_gradient: tf.Tensor
+                The $\frac{\partial L}{\partial z^{\alpha}(t_N)}$ in the paper.
+                The gradient of loss by the final output of the Neural ODE
+                (i.e. by the `final_state`).
+        Returns: Tuple[tf.Tensor, tf.Tensor, List[tf.Tensor]]
+            For the initial state, the gradient of loss by the initial state,
+            and the gradient of loss by the variables `variables`. In the
+            paper, they are $z(t_0)$, $\partial L / \partial z^{\alpha}(t_0)$,
+            and $\partial L / \partial \theta_i^{\alpha}$, respectively.
     """
-    # collect the axis that are not for batch dimension
-    non_batch_axis = [i for i, _ in enumerate(final_state.shape)][1:]
 
-    def contract(adjoint, jacob, is_var):
-        r"""$a_{\alpha} \frac{\partial f^{\alpha}}{\partial b^{\beta}}$
-        where $b$ can either be $h$ or $\theta_i$."""  # XXX: wrong.
-        extra_rank = len(jacob.shape) - len(adjoint.shape)
-        adjoint = tf.reshape(adjoint, (adjoint.shape.as_list() +
-                                       [1] * extra_rank))
-        if is_var:
-            axis = [0] + non_batch_axis
-        else:
-            axis = non_batch_axis
-        return tf.reduce_sum(adjoint * jacob, axis)
-
+    @tf.function
     def aug_dynamics(phase_point, time):
-        state, adjoint, *_ = phase_point  # adjoint: [B, D]
+        state, adjoint, *_ = phase_point
 
         with tf.GradientTape() as g:
             g.watch(state)
@@ -94,17 +79,23 @@ def reverse_mode_derivative(ode_solver,
         # `tf.gradients` or `g.gradient`, if the third argument is filled,
         # returns the vector-Jacobian-products directly. In fact, TF
         # implements VJP inside, and compute gradients via VJP.
-        grads = g.gradient(output, [state] + var_list,
-                           output_gradients=adjoint)
+        grads = g.gradient(output, [state] + variables, -1 * adjoint)
 
         new_phase_point = [output] + grads
         return new_phase_point
 
-    final_phase_point = [final_state, loss_gradient]
-    for var in var_list:
-        zeros = tf.zeros_like(var)
-        final_phase_point.append(zeros)
-    ode_final_value = ode_solver(
-        aug_dynamics, end_time, start_time, final_phase_point)
-    init_state, init_loss_gradient, *grad_loss_by_vars = ode_final_value
-    return grad_loss_by_vars
+    forward = ode_solver(aug_dynamics)
+
+    @tf.function
+    def backward(start_time, end_time, final_state, final_loss_gradient):
+        final_phase_point = [final_state, final_loss_gradient]
+        for var in variables:
+            zeros = tf.zeros_like(var)
+            final_phase_point.append(zeros)
+        ode_final_value = forward(start_time=end_time,
+                                  end_time=start_time,
+                                  initial_phase_point=final_phase_point)
+        init_state, init_loss_gradient, *grad_loss_by_vars = ode_final_value
+        return init_state, init_loss_gradient, grad_loss_by_vars
+
+    return backward
