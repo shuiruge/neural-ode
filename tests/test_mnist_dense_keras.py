acd714f214c42ce1dbb87e71f9c68673fd76c494
shuiruge <shuiruge@hotmail.com>
2019-12-21 02:12:59 +0800

    test on mnist dataset with dense layer

---
+++ tests/test_mnist_dense_keras.py
@@ -0,0 +1,60 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from node.fix_grid import RKSolver
+from node.wrapper import get_node_function
+
+
+class MyLayer(tf.keras.layers.Layer):
+
+    def __init__(self, dt, num_grids, **kwargs):
+        super().__init__(**kwargs)
+        self.dt = dt
+        self.num_grids = num_grids
+
+        t0 = tf.constant(0.)
+        self.tN = t0 + num_grids * dt
+
+        input_dim = 28 * 28
+        self._model = tf.keras.Sequential([
+            tf.keras.layers.Dense(32, activation='relu'),
+            tf.keras.layers.Dense(input_dim, activation='relu'),
+        ])
+        self._model.build([None, input_dim])
+
+        def fn(t, x):
+            return self._model(x)
+
+        self._node_fn = get_node_function(RKSolver(self.dt), 0., fn)
+
+    def call(self, x):
+        y = self._node_fn(self.tN, x)
+        return y
+
+
+def process(X, y):   
+    X = X / 255.
+    X = tf.reshape(X, [-1, 28 * 28])
+    y = tf.one_hot(y, 10)
+    return tf.cast(X, tf.float64), tf.cast(y, tf.float64)
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, y_train = process(x_train, y_train)
+x_test, y_test = process(x_test, y_test)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Input([28 * 28]),
+    MyLayer(dt=1e-2, num_grids=100),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss='categorical_crossentropy',
+    metrics=['accuracy'])
+model.summary()
+
+model.fit(x_train, y_train,
+          epochs=10,
+          validation_data=(x_test, y_test))
