ad5b061bd9659875b06b1a2d40aa1c0f80ef61d2
shuiruge <shuiruge@hotmail.com>
2020-01-03 17:31:30 +0800

    Create

---
+++ tests/test_multi_inputs.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from node.wrapper import node_wrapper
+from node.fix_grid import RKSolver
+
+
+x = tf.constant([[2., 0.]])
+x2 = tf.constant([[1.]])
+# x = tf.constant([[-0.1, 2.0], [-2.0, -0.1]])
+
+solver = RKSolver(0.1)
+dense = tf.keras.layers.Dense(2)
+
+
+@node_wrapper(solver, 0.)
+@tf.function
+def f(t, x):
+    print('------------------', x)
+    x0, x1 = tf.unstack(x[0], axis=-1)
+    y0 = x0 + 2 * x1
+    y1 = -3 * x0 + x1
+    return [dense(tf.stack([y0, y1], axis=-1)), x[1]]
+
+
+with tf.GradientTape() as g:
+    g.watch(x)
+    y = f(1., [x, x2])
+    print(']]]]]]]]]]]]]]]]', y)
+print(g.gradient(y[0], dense.trainable_variables))
