Trying to fix the numerical instabiliby (dense does not have). Still fixing.

node/fix_grid.py
@@ -20,23 +20,35 @@ class FixGridODESolver(ODESolver):
     Returns: ODESolver
     """
 
-    def __init__(self, step_fn, diff_time):
+    def __init__(self, step_fn, diff_time, dtype=tf.float32):
         self.step_fn = step_fn
-        self.diff_time = diff_time
+        self.diff_time = tf.convert_to_tensor(diff_time)
+        self.dtype = dtype
 
     def __call__(self, phase_vector_field):
         step_forward = self.step_fn(phase_vector_field)
 
         @tf.function
+        def cast(x, dtype):
+            return nest_map(lambda x: tf.cast(x, dtype), x)
+
+        @tf.function
         def forward(start_time, end_time, initial_phase_point):
-            t0, tN, x = start_time, end_time, initial_phase_point
-            dt = self.diff_time
+            input_dtype = start_time.dtype
+
+            t0 = cast(start_time, self.dtype)
+            tN = cast(end_time, self.dtype)
+            x = cast(initial_phase_point, self.dtype)
+            dt = cast(self.diff_time, self.dtype)
 
             if tN == t0:
+                x = cast(x, start_time.dtype)
                 return x
 
             if tf.abs(tN - t0) < dt:
-                return step_forward(t0, tN - t0, x)
+                x = step_forward(t0, tN - t0, x)
+                x = cast(x, start_time.dtype)
+                return x
 
             # TODO: illustrate why the `dt` shall be computed as so
             dt = tf.where(tN > t0, dt, -dt)
@@ -45,10 +57,12 @@ class FixGridODESolver(ODESolver):
             # of using `tf.range`, since `tf.range(t0, tN, dt)` will raise
             # error when `t0 > tN`, which will happen in the backward process.
             # However, `tf.linspace` works well in this case.
-            N = tf.cast((tN - t0) / dt + 1, tf.int32)
+            N = int((tN - t0) / dt + 1)
 
             for t in tf.linspace(t0, tN, N):
                 x = step_forward(t, dt, x)
+
+            x = cast(x, input_dtype)
             return x
 
         return forward
@@ -56,8 +70,8 @@ class FixGridODESolver(ODESolver):
 
 class RKSolver(FixGridODESolver):
 
-    def __init__(self, diff_time):
-        super().__init__(rk4_step_fn, diff_time)
+    def __init__(self, diff_time, **kwargs):
+        super().__init__(rk4_step_fn, diff_time, **kwargs)
 
 
 def euler_step_fn(f):