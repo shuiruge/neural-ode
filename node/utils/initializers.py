62915c2da8a5d2a727290f02865603e5dc06d0e5
shuiruge <shuiruge@hotmail.com>
2020-01-09 17:37:03 +0800

    Finize code.

--- node/utils/initializers.py
+++ node/utils/initializers.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 class GlorotUniform(tf.keras.initializers.GlorotUniform):
     """Glorot uniform initializer with scale kwarg."""
 
-    def __init__(self, scale=None, seed=None):
-        super().__init__(seed)
-        self.scale = 1 if scale is None else scale
+    def __init__(self, scale=None, **kwargs):
+        super().__init__(**kwargs)
+        scale = 1 if scale is None else scale
+        self.scale = tf.convert_to_tensor(scale)
 
     def __call__(self, *args, **kwargs):
         return self.scale * super().__call__(*args, **kwargs)
